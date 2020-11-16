f1a1cd86ef61c1244d9b76acb158f3b7f3964f88
Vyacheslav Vaschenok <vyacheslav.vaschenok@nike.com>
2020-11-17 00:33:38 +0300

    fix format

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -32,21 +32,25 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
 
   const uploadFile = async (e: any) => {
       // Get the presigned URL
-      const response = await axios({
-        method: 'GET',
-        url,
-        params: {
-          name: encodeURIComponent(file.name)
-        }
-      })
-      console.log('File to upload: ', file.name)
-      console.log('Uploading to: ', response.data)
-      const result = await fetch(response.data, {
-        method: 'PUT',
-        body: file
-      })
-      console.log('Result: ', result)
-      setFile('');
+      try {
+        const response = await axios({
+          method: 'GET',
+          url,
+          params: {
+            name: encodeURIComponent(file.name)
+          }
+        })
+        console.log('File to upload: ', file.name)
+        console.log('Uploading to: ', response.data.signedUrl)
+        const result = await fetch(response.data.signedUrl, {
+          method: 'PUT',
+          body: file
+        })
+        console.log('Result: ', result)
+        setFile('');
+      } catch (err) {
+        console.log(err)
+      }
     }
   ;
 
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://zbw8eh1lt1.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'hhttps://zbw8eh1lt1.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://zbw8eh1lt1.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://z6b9l9qw3e.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://zbw8eh1lt1.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
